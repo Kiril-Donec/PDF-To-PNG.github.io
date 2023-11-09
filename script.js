a3f17935104e9ad0d58fb3bf3b9dae2a1ad88272
Kiril-Donec <143793718+Kiril-Donec@users.noreply.github.com>
2023-11-09 17:10:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,29 +11,45 @@ document.addEventListener('DOMContentLoaded', () => {
     let pdf = null;
     let currentPage = 1;
 
+    const renderPage = async (pageNumber) => {
+        if (!pdf) return;
+        const page = await pdf.getPage(pageNumber);
+        const scale = 1;
+        const viewport = page.getViewport({ scale });
+        const canvas = pdfCanvas;
+        const context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        const renderContext = {
+            canvasContext: context,
+            viewport: viewport
+        };
+        await page.render(renderContext).promise;
+        pdfPageInfo.textContent = `Сторінка ${pageNumber} з ${pdf.numPages}`;
+        currentPage = pageNumber;
+    };
+
     pdfInput.addEventListener('change', async (e) => {
         const pdfFile = e.target.files[0];
         const pdfjsLib = window['pdfjs-dist/build/pdf'];
         pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.9.359/pdf.worker.min.js';
 
         pdf = await pdfjsLib.getDocument(URL.createObjectURL(pdfFile)).promise;
-        pdfNavButtons.style.display = 'flex';
-        updatePageInfo();
 
-        renderPage(currentPage);
+        renderPage(1);
+
+        pdfNavButtons.style.display = 'flex';
     });
 
     pdfPrevPage.addEventListener('click', () => {
         if (currentPage > 1) {
-            currentPage--;
-            renderPage(currentPage);
+            renderPage(currentPage - 1);
         }
     });
 
     pdfNextPage.addEventListener('click', () => {
         if (currentPage < pdf.numPages) {
-            currentPage++;
-            renderPage(currentPage);
+            renderPage(currentPage + 1);
         }
     });
 
@@ -47,9 +63,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const folder = zip.folder('images');
 
         for (let i = 1; i <= pdf.numPages; i++) {
-            renderPage(i); // Render the page before downloading
             const page = await pdf.getPage(i);
-            const scale = 1.5;
+            const scale = 1;
             const viewport = page.getViewport({ scale });
             const canvas = document.createElement('canvas');
             const context = canvas.getContext('2d');
@@ -82,37 +97,28 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const pagesToDownload = pageInput.value.split(',');
-        const invalidPages = pagesToDownload.some((page) => {
-            const pageNumber = parseInt(page);
-            return isNaN(pageNumber) || pageNumber < 1 || pageNumber > pdf.numPages;
-        });
-
-        if (invalidPages) {
-            alert('Неправильний формат введення сторінок.');
-            return;
-        }
-
         const zip = new JSZip();
         const folder = zip.folder('images');
 
-        for (const page of pagesToDownload) {
-            const pageNumber = parseInt(page);
-            renderPage(pageNumber); // Render the page before downloading
-            const page = await pdf.getPage(pageNumber);
-            const scale = 1.5;
-            const viewport = page.getViewport({ scale });
-            const canvas = document.createElement('canvas');
-            const context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-            const renderContext = {
-                canvasContext: context,
-                viewport: viewport
-            };
-            await page.render(renderContext).promise;
-            const imgData = canvas.toDataURL('image/png');
-            const imgName = `page_${pageNumber}.png`;
-            folder.file(imgName, imgData.split('base64,')[1], { base64: true });
+        for (const pageToDownload of pagesToDownload) {
+            const pageNumber = parseInt(pageToDownload);
+            if (!isNaN(pageNumber) && pageNumber >= 1 && pageNumber <= pdf.numPages) {
+                const page = await pdf.getPage(pageNumber);
+                const scale = 1;
+                const viewport = page.getViewport({ scale });
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+                const renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                };
+                await page.render(renderContext).promise;
+                const imgData = canvas.toDataURL('image/png');
+                const imgName = `page_${pageNumber}.png`;
+                folder.file(imgName, imgData.split('base64,')[1], { base64: true });
+            }
         }
 
         zip.generateAsync({ type: 'blob' })
@@ -120,28 +126,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 const zipUrl = URL.createObjectURL(blob);
                 const zipLink = document.createElement('a');
                 zipLink.href = zipUrl;
-                zipLink.download = 'images.zip';
+                zipLink.download = 'selected_images.zip';
                 zipLink.click();
             });
     });
-
-    function renderPage(pageNumber) {
-        pdf.getPage(pageNumber).then((page) => {
-            const scale = 1.5;
-            const viewport = page.getViewport({ scale });
-            pdfCanvas.height = viewport.height;
-            pdfCanvas.width = viewport.width;
-            const context = pdfCanvas.getContext('2d');
-            const renderContext = {
-                canvasContext: context,
-                viewport: viewport
-            };
-            pdfPageInfo.textContent = `Сторінка ${pageNumber} з ${pdf.numPages}`;
-            page.render(renderContext).promise;
-        });
-    }
-
-    function updatePageInfo() {
-        pdfPageInfo.textContent = `Сторінка ${currentPage} з ${pdf.numPages}`;
-    }
 });
