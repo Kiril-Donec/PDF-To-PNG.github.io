37f588c51f885137e0b1f51837bb4e639e8e406f
Kiril-Donec <143793718+Kiril-Donec@users.noreply.github.com>
2023-11-09 15:11:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const downloadSelectedButton = document.getElementById('downloadSelected');
     const pageInput = document.getElementById('pageInput');
     const pdfPreview = document.getElementById('pdfPreview');
-    const pageSelection = document.getElementById('pageSelection');
+    const pageInfo = document.getElementById('pageInfo');
     let pdf = null;
 
     pdfInput.addEventListener('change', async (e) => {
@@ -14,11 +14,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
         pdf = await pdfjsLib.getDocument(URL.createObjectURL(pdfFile)).promise;
 
+        const firstPage = await pdf.getPage(1);
+        const scale = 1.5;
+        const viewport = firstPage.getViewport({ scale });
+        const canvas = document.createElement('canvas');
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+        const context = canvas.getContext('2d');
+        const renderContext = {
+            canvasContext: context,
+            viewport: viewport
+        };
+        await firstPage.render(renderContext).promise;
+        pdfPreview.innerHTML = '';
+        pdfPreview.appendChild(canvas);
+
         const numPages = pdf.numPages;
-        const pagesLabel = document.createElement('label');
-        pagesLabel.textContent = `Виберіть сторінки для завантаження (1-${numPages}):`;
-        pageSelection.insertBefore(pagesLabel, pageInput);
-        pageInput.placeholder = `1-${numPages}`;
+        pageInfo.textContent = `Сторінка 1 з ${numPages}`;
     });
 
     downloadAllButton.addEventListener('click', async () => {
@@ -27,44 +39,17 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        downloadPages(1, pdf.numPages);
-    });
-
-    downloadSelectedButton.addEventListener('click', async () => {
-        if (!pdf) {
-            alert('Спершу виберіть PDF-файл.');
-            return;
-        }
-
-        const pagesToDownload = pageInput.value.split('-');
-        if (pagesToDownload.length !== 2) {
-            alert('Неправильний формат введення сторінок.');
-            return;
-        }
-
-        const startPage = parseInt(pagesToDownload[0]);
-        const endPage = parseInt(pagesToDownload[1]);
-
-        if (isNaN(startPage) || isNaN(endPage) || startPage < 1 || endPage < 1 || startPage > endPage || endPage > pdf.numPages) {
-            alert('Неправильний діапазон сторінок.');
-            return;
-        }
-
-        downloadPages(startPage, endPage);
-    });
-
-    async function downloadPages(start, end) {
         const zip = new JSZip();
         const folder = zip.folder('images');
 
-        for (let i = start; i <= end; i++) {
+        for (let i = 1; i <= pdf.numPages; i++) {
             const page = await pdf.getPage(i);
             const scale = 1.5;
             const viewport = page.getViewport({ scale });
             const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
             canvas.height = viewport.height;
             canvas.width = viewport.width;
-            const context = canvas.getContext('2d');
             const renderContext = {
                 canvasContext: context,
                 viewport: viewport
@@ -83,5 +68,50 @@ document.addEventListener('DOMContentLoaded', () => {
                 zipLink.download = 'images.zip';
                 zipLink.click();
             });
-    }
+    });
+
+    downloadSelectedButton.addEventListener('click', async () => {
+        if (!pdf) {
+            alert('Спершу виберіть PDF-файл.');
+            return;
+        }
+
+        const pagesToDownload = pageInput.value.split(',').map(Number);
+        const invalidPages = pagesToDownload.filter(page => isNaN(page) || page < 1 || page > pdf.numPages);
+
+        if (invalidPages.length > 0) {
+            alert('Неправильний діапазон сторінок.');
+            return;
+        }
+
+        const zip = new JSZip();
+        const folder = zip.folder('images');
+
+        for (const pageNumber of pagesToDownload) {
+            const page = await pdf.getPage(pageNumber);
+            const scale = 1.5;
+            const viewport = page.getViewport({ scale });
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+            const renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+            await page.render(renderContext).promise;
+            const imgData = canvas.toDataURL('image/png');
+            const imgName = `page_${pageNumber}.png`;
+            folder.file(imgName, imgData.split('base64,')[1], { base64: true });
+        }
+
+        zip.generateAsync({ type: 'blob' })
+            .then((blob) => {
+                const zipUrl = URL.createObjectURL(blob);
+                const zipLink = document.createElement('a');
+                zipLink.href = zipUrl;
+                zipLink.download = 'images.zip';
+                zipLink.click();
+            });
+    });
 });
